1ae4d9cbd82f112938c5fe262ef686ec22757cc7
anu-kriti-Shri-vastava <anukriti13901@gmail.com>
2024-04-12 11:43:22 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,13 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
